b5e1abdb331f6ab49de98a9c5784179eb9a4433e
AlexisMic <amschotte@me.com>
2023-12-22 12:26:16 +0000

    Initial Commit

---
+++ BluetoothConnector/BluetoothConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothConnectorApp.swift
+//  BluetoothConnector
+//
+//  Created by Alexis Schotte on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
